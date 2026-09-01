5aa7912f1102ca5d9a0c5c198b5e2bd81e1b0e31
guilherme-messer <guilhermemesser@gmail.com>
2026-09-01 18:41:56 -0300

    feat: add tab bar component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabBar, TabKey } from './src/components/TabBar';
 
 export default function App() {
+  const [activeTab, setActiveTab] = useState<TabKey>('jornada');
+
   return (
     <View style={styles.container}>
-      <Text>Silpo Mobile</Text>
+      <View style={styles.content}>
+        <Text>Silpo Mobile</Text>
+      </View>
+      <TabBar activeTab={activeTab} onTabPress={setActiveTab} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,6 +21,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  content: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
