65908da5d0f3b98c3f7decf1728f385f95f76257
Gabriel-Brandt <gabibrandtbolado@gmail.com>
2026-09-09 18:31:33 -0300

    adicao de comentarios

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <DailyPractice></DailyPractice>
+      {/*<DailyPractice></DailyPractice>*/}
       <StatusBar style="dark" />
     </View>
   );
