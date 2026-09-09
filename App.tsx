afae614950ad12699756bc2c4cc42d57ec316910
Fernando <fernandopgb02@gmail.com>
2026-09-09 19:37:58 -0300

    fix/removi a screen de dentro do App.tsx

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import { useAppFonts } from '@/hooks/useAppFonts';
 import { colors, fontFamily, fontSize } from '@/theme';
 import { StatusBar } from 'expo-status-bar';
-import UserProfileScreen from '@/features/profileScreen/UserProfileScreen';
 
 export default function App() {
   const { isLoaded, error } = useAppFonts();
@@ -14,7 +13,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Silpo Mobile</Text>
-      <UserProfileScreen></UserProfileScreen>
       <StatusBar style="dark" />
     </View>
   );
